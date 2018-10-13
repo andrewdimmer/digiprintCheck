6a534ff6f9ea43d0ea912f5cd223690427f43df4
andrewdimmer <43498863+andrewdimmer@users.noreply.github.com>
2018-10-13 07:30:43 -0400

    Added Logout feature
    Added the logout function
    Also switched to an event listen to determine if the user is logged in

--- firebase-digiprintcheck.js
+++ firebase-digiprintcheck.js
@@ -1,17 +1,26 @@
 // Checks if the user is logged in or not
 // Displays the login link if they are not
 // Displays the profile link if they are
-function isLoggedIn() {
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in.
-            document.getElementById("lptext").innerHTML = "Profile";
-            document.getElementById("lphref").href = "../profile.html";
-            document.getElementById("signout").style.display = "block";
-        } else {
-            document.getElementById("lptext").innerHTML = "Login";
-            document.getElementById("lphref").href = "../login.html";
-            document.getElementById("signout").style.display = "none";
-        }
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        console.log("User is logged in");
+        document.getElementById("lptext").innerHTML = "Profile";
+        document.getElementById("lphref").href = "../profile.html";
+        document.getElementById("logout").style.display = "block";
+    } else {
+        console.log("User is not logged in");
+        document.getElementById("lptext").innerHTML = "Login";
+        document.getElementById("lphref").href = "../login.html";
+        document.getElementById("logout").style.display = "none";
+    }
+});
+
+// Logs the user out
+function logout() {
+    firebase.auth().signOut().then(function() {
+        console.log('Signed Out');
+    }, function(error) {
+        console.error('Sign Out Error', error);
     });
 }
