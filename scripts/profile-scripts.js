b94368c91b8e5f65e5a23acd822562b2262b0684
andrewdimmer <43498863+andrewdimmer@users.noreply.github.com>
2018-10-13 08:41:28 -0400

    Reorganized file structure, created profile page
    Reorganized the file structure to include directories then updated all the relevant pages to reflect that change.
    Added the basic profile page, including all required firebase functions. Still need to add page specific functions to actually call the firebase functions added in this commit.

---
+++ scripts/profile-scripts.js
@@ -0,0 +1,48 @@
+//Profile page Firebase functions
+function getInfo(user) {
+    if (user != null) {
+        console.log("Display user profile information");
+        document.getElementById("userName").innerHTML = user.displayName;
+        document.getElementById("userEmail").innerHTML = user.email;
+        document.getElementById("userPassword").innerHTML = "********";
+    } else {
+        // Error handling
+        console.log("No user logged in");
+    }
+}
+
+function updateName() {
+    var user = firebase.auth().currentUser;
+    var newName = document.getElementById("newName").value;
+
+    user.updateProfile({
+        displayName: newName,
+    }).then(function() {
+        // Update successful.
+    }).catch(function(error) {
+        // An error happened.
+    });
+}
+
+function updateEmail() {
+    var user = firebase.auth().currentUser;
+    var newEmail = document.getElementById("newEmail").value;
+
+    user.updateEmail(newEmail).then(function() {
+        // Update successful.
+    }).catch(function(error) {
+        // An error happened.
+    });
+}
+
+function updatePassword() {
+    var user = firebase.auth().currentUser;
+    var newPassword = document.getElementById("newPassword").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+    user.updatePassword(newPassword).then(function() {
+        // Update successful.
+    }).catch(function(error) {
+        // An error happened.
+    });
+}
