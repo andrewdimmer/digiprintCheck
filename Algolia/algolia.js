e4756131af8cb80017942e3070c637ba82cfe511
root <root@DESKTOP-CG858GS.localdomain>
2018-10-13 20:22:33 -0400

    Debug select

--- Algolia/algolia.js
+++ Algolia/algolia.js
@@ -20,16 +20,62 @@ var index = client.initIndex('your_index_name');
      customRanking: ['desc(popularity)'],
    });
 
-  var search = instantsearch({
-    // Replace with your own values
-    appId: 'VGXKB5XFTW',
-    apiKey: 'aaa793df40bb67158d9188458b585930', // search only API key, no ADMIN key
-    indexName: 'instant_search',
-    urlSync: true,
-    searchParameters: {
-      hitsPerPage: 10
-    }
-  });
+   // Search the "category" facet for values matching "phone" in records
+// having "Apple" in their "brand" facet.
+index.searchForFacetValues({
+  facetName: 'category',
+  facetQuery: 'phone',
+  filters: 'brand:apple'
+}, function(err, content) {
+  if (err) throw err;
+
+  console.log(content.facetHits);
+});
+
+
+  // var search = instantsearch({
+  //   // Replace with your own values
+  //   appId: 'VGXKB5XFTW',
+  //   apiKey: 'aaa793df40bb67158d9188458b585930', // search only API key, no ADMIN key
+  //   indexName: 'instant_search',
+  //   urlSync: true,
+  //   searchParameters: {
+  //     hitsPerPage: 10
+  //   }
+  // });
+
+  // var search = instantsearch({
+  //   [...],
+  //   searchParameters: {
+  //     hierarchicalFacetsRefinements: { // menu is implemented as a hierarchicalFacetsRefinements
+  //       categories: ['Cell Phones']
+  //     },
+  //     facetsRefinements: {
+  //       in_stock: [true]
+  //     },
+  //     // Add to "facets" all attributes for which you
+  //     // do NOT have a widget defined
+  //     facets: ['in_stock']
+  //   },
+  // });
+  // Below is just a common widget configuration, to show
+  // how it interacts with the above searchParameters
+  search.addWidget(
+    instantsearch.widgets.menu({
+      [...],
+      attributeName: 'categories'
+    })
+  );
+
+  
+    // /* Filtering and Faceting */
+
+    // index.search({
+    //   query: 'query',
+    //   filters: '(category:Book OR category:Ebook) AND _tags:published'
+    // }).then(res => {
+    //   // console.log(res);
+    // });
 
   // Add this after the previous JavaScript code
 search.addWidget(
@@ -49,12 +95,41 @@ search.addWidget(
   })
 );
 
-// Add this after the other search.addWidget() calls
+// search.addWidget(
+//   instantsearch.widgets.refinementList({
+//     container: '#brand-refinement',
+//     attributeName: 'brand',
+//     templates: {
+//       header: 'Brand'
+//     },
+//     searchForFacetValues: {
+//       placeholder: 'Search for brands',
+//       templates: {
+//         noResults: '<div class="sffv_no-results">No matching brands.</div>'
+//       }
+//     }
+//   })
+// );
+
+search.addWidget(
+   instantsearch.widgets.sortBySelector({ 
+  container: string|HTMLElement, 
+  indices: Array<SortByIndexDefinition>, 
+  autoHideContainer: [boolean], 
+  cssClasses: [SortByWidgetCssClasses], 
+  transformItems: [(Array<object>) => Array<object>], 
+ SortByWidgetOptions})
+);
+
+
+
+
+//Add this after the other search.addWidget() calls
 search.addWidget(
     instantsearch.widgets.pagination({
       container: '#pagination'
     })
   );
 
-  // Add this after all the search.addWidget() calls
+//Add this after all the search.addWidget() calls
 search.start();
